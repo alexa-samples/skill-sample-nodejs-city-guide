668b7d7f140fc4447a68a7d2158759fed0af9d0f
Seth <seth.stansberry@gmail.com>
2017-07-15 22:21:57 -0400

    Edited Tell vs Ask Behavior
    - Adjusted feedback of tool to respond to feedback about a bug  reported by Amazon. 
      - App would previously close when you asked it to tell it about Carrboro
      - Now the app remains up and listens afterwards.

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ var welcomeMessage = location + " Guide. You can ask me for an attraction, the l
 
 var welcomeRepromt = "You can ask me for an attraction, the local news, or  say help. What will it be?";
 
-var locationOverview = "Carborro is a city in the North Carolina triangle and is in Orange country. Located directly west of Chapel Hill, home of the University of North Carolina's flagship campus, Carrboro has a reputation as one of the most progressive communities in the Southern United States.  What else would you like to know?";
+var locationOverview = "Carrboro is a city in the North Carolina triangle and is in Orange country. Located directly west of Chapel Hill, home of the University of North Carolina's flagship campus, Carrboro has a reputation as one of the most progressive communities in the Southern United States.  What else would you like to know?";
 
 var HelpMessage = "Here are some things you  can say: Give me an attraction. Tell me about " + location + ". Tell me the top five things to do. Tell me the local news.  What would you like to do?";
 
@@ -24,12 +24,12 @@ var moreInformation = "See your  Alexa app for  more  information."
 
 var tryAgainMessage = "please try again."
 
-var noAttractionErrorMessage = "There was an error finding this attraction, " + tryAgainMessage;
+var noAttractionErrorMessage = "What attraction was that? " + tryAgainMessage;
 
-var topFiveMoreInfo = " You can tell me a number for more information. For example open number one.";
+var topFiveMoreInfo = " You can tell me a number for more information. For example, open number one.";
 
 var getMoreInfoRepromtMessage = "What number attraction would you like to hear about?";
-n
+
 var getMoreInfoMessage = "OK, " + getMoreInfoRepromtMessage;
 
 var goodbyeMessage = "OK, have a nice time in " + location + ".";
@@ -45,20 +45,19 @@ var output = "";
 var alexa;
 
 var attractions = [
-    { name: "The Woodland Park Zoo", content: "located just 10 minutes north of downtown Seattle. The zoo's 92-acres and award-winning exhibits are home to more than 1,000 animals representing 300 species from around the world.", location: "There are two zoo entrances. \n West Entrance:\n Cross streets: Phinney Ave. N. between N. 55th St. & N. 56th St.\n Street address: 5500 Phinney Ave. N., Seattle WA 98103\n South Entrance:\n Cross streets: N. 50th Street & Fremont Ave. N.\n Street address: 750 N. 50th Street, Seattle WA 98103", contact: "zooinfo@zoo.org\n 206 548 2500" },
-    { name: "The EMP Museum", content: "Dedicated to contemporary popular culture, the EMP Museum was established by Microsoft co-founder Paul Allen in 2000. It's home to exhibits, interactive activity stations, sound sculpture, and various educational resources.", location: "325 5th Avenue N, Seattle, Washington", contact: "206 770 2700" },
-    { name: "Waterfront Park", content: "Designed by the Bumgardner Partnership and consultants, Waterfront Park is a public park constructed on the site of the former Schwabacher Wharf. you can enjoy excellent views of the surrounding arey, such as the city skyline, ships in drydock, container cranes and the West Seattle Bridge.", location: "1401 Alaskan Way, Seattle, WA 98101, United States", contact: "206 684 4075" },
-    { name: "Chihuly Garden and Glass", content: "Opened in 2012 on the former site of the Fun Forest, Chihuly Garden and Glass is an exhibit showcasing the work of Dale Chihuly. It comprises of three primary components: the Garden, the Glasshouse, and the Interior Exhibit. There is also a 90 seat cafe with additional outdoor dining.", location: "305 Harrison St, Seattle, WA 98109, United States", contact: "206 753 4940" },
-    { name: "Woodland Park", content: "A 90 acre public park home to many species of birds and mammals. Woodland park boasts several picnic areas, a formal rose garden, ballfields, a miniature golf range and a play area for children.", location: "1000 N 50th St, Seattle, WA 98103, United States", contact: "206 684 4075" },
+    { name: "Weaver Street Lawn", content: "Weaver Street Market,  a community owned grocery, has three locations and all host free live music events on Sundays.", location: "There are three locations. \n Carrboro:\n Street Address: 101 East Weaver Street\n Hillsborough\n Street Address: 228 South Churton Street \n Chapel Hill\n Street address: 716 Market Street\n", contact: "919 929 0010" },
+    { name: "Farmers Market", content: "Touted as one of the finest local markets in the country, the Carrboro Farmers' Market has provided an opportunity for local farmers to sell their products directly to the consumer since 1979.", location: "Street Address: 301 W Main St\n", contact: "919 280 3326" },
+    { name: "Cat's Cradle", content: "The Cat's Cradle is a music venue located in Carrboro. The Cat's Cradle hosts up and coming indie music acts, but also hosts established acts- including Nirvana, Pearl Jam, and Public Enemy. Tickets are very affordable and range from 8 to 20 dollars a piece.", location: "Street Address: 300 E Main Street\n", contact: "206 684 4075" },
+    { name: "The Arts Center", content: "For thirty years the ArtsCenter in Carrboro has functioned as a dynamic artistic center for the community. It features a nonstop calendar of art classes, world-music, theater, children and family programming, and art exhibits. The facilities include a theater, classrooms, a dance studio, an informal performance space, and an art gallery.", location: "Street Address:\n 300-G East Main Street\n", contact: "919 929 2787" },
+    { name: "The Taco Trucks!", content: "Carrboro has a taco truck on every corner. Tacos are delecious- and anyone who has eaten a taco from a taco truck can attest that they produce the world's best tacos.", location: "Street Address: Every Corner", contact: "1-800-TACO\n Warning: Not actually how you contact a taco truck.\n Taco trucks are decentralized and do not share a single number." },
 ];
-
 
 var topFive = [
     { number: "1", caption: "Live and breathe the local food scene!", more: "Carrboro has an ecclectic and exciting local food and drink scene. Visit the critically acclaimed local resturants such as ACME and Pizzeria Mercato. After eating to satiation, have a night cap by visiting the local bars and asking for a pickleback- a local speciality drink.", location: "Mercato: \n408 W Weaver St, Carrboro, NC\nBaxter Barcade:\n 108 N Graham St, Carrboro, NC 27510\n", contact: "Mercato: 919 967 2277\nBaxter: 919 929 2263\n" },
     { number: "2", caption: "Run free at the local greenways.", more: "North Carolina is absolutely beautiful. One of the best ways to experience nature is through the parks system. Carrboro is one of the most pedestrian friendly areas of the entire state, and it shows through their paved and unpaved greenways and park system. When you're in the area- ensure you make time to explore the nature.", location: "Miscellaneous: Review the websites of Carrboro Parks and Recreation for more information", contact: "919 918 4475" },
     { number: "3", caption: "Geek out at Baxter and put your quarters to good use.", more: "Baxter Barcade is a staple for hip people in Carrboro. When walking in, patrons are greeted with the sounds of classic arcades, pinball flippers, and nostalgic flashign lights. Games are only 25 cents a pop, and Baxter often hosts community events like The Walking Dead night.", location: "108 N Graham St, Carrboro, NC 27510", contact: "919 869 7486" },
-    { number: "4", caption: "Get caught up in the rivalry watching a Tar Heels game.", more: "The athletic rivalry between the Duke Blue Devils and North carolina tarheels is considered one of the most intense rivalries in all of sports according to a poll conducted by Sports Illustrated. Visit Franklin Street during a game night and you are guaranteed to be in the midst of hundreds of UNC students and enjoy the energy of the basketball game.", location: "Anywhere on Franklin Street", contact: "Anyone At UNC Chapel Hill" },
-    { number: "5", caption: "Maximize your time giving back to the community by volunteering.", more: "Carrobo is a small town with a tight knight community. Find a non profit that really is powerful to you and make some time to make a difference in the community around you. ReCYCLEry and Weaver Street Market are both great places to start volunteering. Both coops are community owned and are geared towards making people empowered and partner up with disabled communities. Making positive impactful changes on the community around you embodies the Tarheel spirit.", location: "ReCYCLEry: \n108 N Graham St, Chapel Hill, NC 27516\nWeaver Street Market: 716 Market St, Chapel Hill, NC 27516\n", contact: "ReCYCLEry:\nbikes@recyclery.org\nWeaver Street Market: tablenc@gmail.com" }
+    { number: "4", caption: "Get caught up in the rivalry watching a Tar Heels game.", more: "Get caught up in the rivalry watching a Tar Heels game.", more: "The athletic rivalry between the Duke Blue Devils and North carolina tarheels is considered one of the most intense rivalries in all of sports according to a poll conducted by Sports Illustrated. Visit Franklin Street during a game night and you are guaranteed to be in the midst of hundreds of UNC students and enjoy the energy of the basketball game.", location: "Anywhere on Franklin Street", contact: "Anyone At UNC Chapel Hill" },
+    { number: "5", caption: "Maximize your time giving back to the community by volunteering.", more: "Carrobo is a small town with a tight knight community. Find a non profit that really is powerful to you and make some time to make a difference in the community around you. ReCYCLEry and Weaver Street Market are both great places to start volunteering. Both coops are community owned and are geared towards making people empowered and partner up with disabled communities. Making positive impactful changes on the community around you embodies the Tarheel spirit.", location: "ReCYCLEry: \n108 N Graham St, Chapel Hill, NC 27516\nWeaver Street Market: 716 Market St, Chapel Hill, NC 27516", contact: "ReCYCLEry:\nbikes@recyclery.org Weaver Street Market: tablenc@gmail.com" }
 ];
 
 var topFiveIntro = "Here are the top five things to  do in " + location + ".";
@@ -69,6 +68,14 @@ var newSessionHandlers = {
         output = welcomeMessage;
         this.emit(':ask', output, welcomeRepromt);
     },
+    'getOverview': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getOverview');
+    },
+    'getNewsIntent': function () {
+        this.handler.state = states.SEARCHMODE;
+        this.emitWithState('getNewsIntent');
+    },
     'getAttractionIntent': function () {
         this.handler.state = states.SEARCHMODE;
         this.emitWithState('getAttractionIntent');
@@ -77,7 +84,7 @@ var newSessionHandlers = {
         this.handler.state = states.SEARCHMODE;
         this.emitWithState('getTopFiveIntent');
     },
-e    'AMAZON.StopIntent': function () {
+    'AMAZON.StopIntent': function () {
         this.emit(':tell', goodbyeMessage);
     },
     'AMAZON.CancelIntent': function () {
@@ -87,7 +94,7 @@ e    'AMAZON.StopIntent': function () {
     'SessionEndedRequest': function () {
         // Use this function to clear up and save any data needed between sessions
         this.emit('AMAZON.StopIntent');
-l    },
+    },
     'Unhandled': function () {
         output = HelpMessage;
         this.emit(':ask', output, welcomeRepromt);
@@ -97,7 +104,7 @@ l    },
 var startSearchHandlers = Alexa.CreateStateHandler(states.SEARCHMODE, {
     'getOverview': function () {
         output = locationOverview;
-/        this.emit(':askWithCard', output, location, locationOverview);
+        this.emit(':askWithCard', output, output, locationOverview);
     },
     'getAttractionIntent': function () {
         var cardTitle = location;
@@ -109,7 +116,7 @@ var startSearchHandlers = Alexa.CreateStateHandler(states.SEARCHMODE, {
             cardTitle = attraction.name;
             cardContent = attraction.content + newline + attraction.contact;
 
-            this.emit(':tellWithCard', output, cardTitle, cardContent);
+            this.emit(':askWithCard', output, cardTitle, cardContent);
         } else {
             this.emit(':ask', noAttractionErrorMessage, tryAgainMessage);
         }
@@ -214,11 +221,18 @@ var topFiveHandlers = Alexa.CreateStateHandler(states.TOPFIVE, {
     },
 
     'getMoreInfoIntent': function () {
-        var slotValue = this.event.request.intent.slots.attraction.value;
-        var index = parseInt(slotValue) - 1;
+        var slotValue = 0;
+        if(this.event.request.intent.slots.attraction ) {
+            if (this.event.request.intent.slots.attraction.value) {
+                slotValue = this.event.request.intent.slots.attraction.value;
 
-        var selectedAttraction = topFive[index];
-        if (selectedAttraction) {
+            }
+        }
+
+        if (slotValue > 0 && slotValue <= topFive.length) {
+
+            var index = parseInt(slotValue) - 1;
+            var selectedAttraction = topFive[index];
 
             output = selectedAttraction.caption + ". " + selectedAttraction.more + ". " + hearMoreMessage;
             var cardTitle = selectedAttraction.name;
@@ -266,10 +280,10 @@ exports.handler = function (event, context, callback) {
 
 // Create a web request and handle the response.
 function httpGet(query, callback) {
-  console.log("/n QUERY: "+query);
+    console.log("/n QUERY: "+query);
 
     var options = {
-      //http://api.nytimes.com/svc/search/v2/articlesearch.json?q=seattle&sort=newest&api-key=
+        //http://api.nytimes.com/svc/search/v2/articlesearch.json?q=seattle&sort=newest&api-key=
         host: 'api.nytimes.com',
         path: '/svc/search/v2/articlesearch.json?q=' + query + '&sort=newest&api-key=' + APIKey,
         method: 'GET'
@@ -277,25 +291,25 @@ function httpGet(query, callback) {
 
     var req = http.request(options, (res) => {
 
-        var body = '';
+            var body = '';
 
-        res.on('data', (d) => {
-            body += d;
-        });
+    res.on('data', (d) => {
+        body += d;
+});
 
-        res.on('end', function () {
-            callback(body);
-        });
-
+    res.on('end', function () {
+        callback(body);
     });
+
+});
     req.end();
 
     req.on('error', (e) => {
         console.error(e);
-    });
+});
 }
 
 String.prototype.trunc =
-      function (n) {
-          return this.substr(0, n - 1) + (this.length > n ? '&hellip;' : '');
-      };
+    function (n) {
+        return this.substr(0, n - 1) + (this.length > n ? '&hellip;' : '');
+};
